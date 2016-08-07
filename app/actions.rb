de592500bce44bf5ec525497793b12e9f9b57458
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-07 04:08:04 +0000

    create initial form

---
+++ app/actions.rb
@@ -0,0 +1,14 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/messages' do
+  @messages = Message.all 
+  erb :'messages/index'
+end 
+
+get '/messages/new' do
+  erb :'messages/new'
+end 
+
